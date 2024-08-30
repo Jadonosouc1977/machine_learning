9e9527409f89430c8653803dffd5d354d79012db
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 06:43:04 -0400

    Update ejercicio_app.py

--- ejercicio_app.py
+++ ejercicio_app.py
@@ -20,9 +20,11 @@ with st.expander('Data'):
 with st.expander('Data visualization:'):
   st.scatter_chart(data=df, x='bill_length_mm',y='body_mass_g',color = 'species')
   
-# Data preparation
+# Data preparationd
 with st.sidebar:
   st.header('Input Features :')
   island = st.selectbox('Island',('Biscoe','Dream','Torgensen'))
   gender = st.selectbox('Gender',('male','female'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1,59.4,43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
+  flipper_length = st.slider('Flipper length (mm)', 172.0,231.0,201.0)
