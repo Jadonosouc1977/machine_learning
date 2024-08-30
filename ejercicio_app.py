79ee2b494ae06d71f81ed3f252bb06607ec6ef09
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 06:37:55 -0400

    Update ejercicio_app.py

--- ejercicio_app.py
+++ ejercicio_app.py
@@ -23,3 +23,6 @@ with st.expander('Data visualization:'):
 # Data preparation
 with st.sidebar:
   st.header('Input Features :')
+  island = st.selectbox('Island',('Biscoe','Dream','Torgensen'))
+  gender = st.selectbox('Gender',('male','female'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1,59.4,43.9)
