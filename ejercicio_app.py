46ae876637353d32d4c7d834fec8bd4ff342268c
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 07:35:03 -0400

    Update ejercicio_app.py

--- ejercicio_app.py
+++ ejercicio_app.py
@@ -20,7 +20,7 @@ with st.expander('Data'):
 with st.expander('Data visualization:'):
   st.scatter_chart(data=df, x='bill_length_mm',y='body_mass_g',color = 'species')
   
-# Data preparationd
+# Data preparation
 with st.sidebar:
   st.header('Input Features :')
   island = st.selectbox('Island',('Biscoe','Dream','Torgensen'))
@@ -41,13 +41,17 @@ with st.sidebar:
   input_df = pd.DataFrame(data,index=[0])
   input_penguins = pd.concat([input_df,X],axis=0)
 
+  # Encode
+  encode = ['island','sex']
+  df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+  input_row = df_penguins[:1]
+
 with st.expander('Input Features:'):
   st.write('**Input penguins**')
   input_df
   st.write('**Combined penguins data**')
   input_penguins
+  st.write('**Encoded input penguis**')
+  input_row
 
-# Encode
-encode = ['island','sex']
-df_penguins = pd.get_dummies(input_penguins, prefix = encode)
-df_penguins[:1]
+
