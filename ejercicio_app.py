d075121a8f164d30d9a903e0fb6586d2e9515de8
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 06:09:05 -0400

    Update ejercicio_app.py

--- ejercicio_app.py
+++ ejercicio_app.py
@@ -4,5 +4,7 @@ import pandas as pd
 st.title('Machine Learning')
 st.info('Ejercicio de entrenamiento')
 
-df = pd.read_csv('https://raw.githubusercontent.com/Jadonosouc1977/machine_learning/main/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data:**')
+  df = pd.read_csv('https://raw.githubusercontent.com/Jadonosouc1977/machine_learning/main/penguins_cleaned.csv')
+  df
