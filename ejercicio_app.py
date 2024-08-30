34861578756efbc8078fd1bfcb974acd7f3a1c9a
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 08:25:18 -0400

    Update ejercicio_app.py

--- ejercicio_app.py
+++ ejercicio_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.emsemble import RandomForestClassifier
 
 st.title('Machine Learning')
 st.info('Ejercicio de entrenamiento')
@@ -53,6 +54,9 @@ with st.expander('Input Features:'):
 # Encode X
 encode = ['island','sex']
 df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+
+X = df_penguins[1:]
+
 input_row = df_penguins[:1]
 
 # Encode y
@@ -69,4 +73,13 @@ with st.expander('Data preparation:'):
   st.write('**Encoded (y) **')
   y
 
+# Model Traning and inference #
+# Train the ML model #
+clf = RandomForest()
+clf.fit(X,y)
 
+# Apply model to make predictions #
+prediction = clf.predict(input_row)
+prediction_proba = clf.predict_proba(input_row)
+
+prediction_proba
