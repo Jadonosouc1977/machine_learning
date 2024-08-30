94349347eb368f4f2891ce1cd348ef5a5a3b4b5b
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 06:00:45 -0400

    Update ejercicio_app.py

--- ejercicio_app.py
+++ ejercicio_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning')
 st.info('Ejercicio de entrenamiento')
+
+df = pd.read_csv('https://github.com/Jadonosouc1977/machine_learning/blob/main/penguins_cleaned.csv')
+df
